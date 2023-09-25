237829fb16302ed49a97a0211bac995508ac4fed
didierMaba <deus_maba@yahoo.fr>
2023-09-25 16:13:39 +0200

    FC

---
+++ index.js
@@ -0,0 +1,30 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const mousses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+  mousses.forEach((mouse) => {
+    mouse.style.left = e.x + "px";
+    mouse.style.top = e.y + "px";
+    
+  });
+});
+
+// window.addEventListener("mousemove", (e) => {
+//     cursor.style.top = e.y + "px";
+//     cursor.style.left = e.x + "px";
+
+//     mouse1.style.top = e.y + "px";
+//     mouse1.style.left = e.x + "px";
+
+//     mouse2.style.top = e.y + "px";
+//     mouse2.style.left = e.x + "px";
+// })
+
+
